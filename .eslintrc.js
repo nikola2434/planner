0875ab2e8e2d43436cf154824bd5602a3a73fe4b
Reviakin Nikolai <reviakin223@gmail.com>
2024-12-04 23:53:56 +0300

    initial fullstack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
 module.exports = {
+	root: true,
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint/eslint-plugin'],
+	plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
 	extends: [
+		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
 		'plugin:prettier/recommended'
 	],
-	root: true,
 	env: {
 		node: true,
-		jest: true
+		browser: true,
+		es2021: true,
+		jest:
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
 	},
 	ignorePatterns: ['.eslintrc.js'],
 	rules: {
@@ -16,7 +28,23 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
-		'no-console': 1,
-		'prettier/prettier': 0
+		'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+		'no-console': 1
+	},
+	overrides: [
+		{
+			files: ['*.js', '*.cjs'], // Для конфигурационных файлов
+			env: {
+				node: true
+			},
+			parserOptions: {
+				sourceType: 'module'
+			}
+		}
+	],
+	settings: {
+		react: {
+			version: 'detect' // Автоматическое определение версии React
+		}
 	}
 }
